afe5114acbeb14d7eafe02fbf5a5596647f042ca
DESKTOP-TCD3F32\jefri <ajefri7608@gmail.com>
2022-03-02 00:30:50 +0800

    init

---
+++ src/main/java/com/example/java_backend_demo/JavaBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_backend_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_backend_demo/JavaBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_backend_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
