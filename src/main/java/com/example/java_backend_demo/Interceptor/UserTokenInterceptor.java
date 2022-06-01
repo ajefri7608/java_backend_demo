90a1cac534a252ff63961ae55df49715b0d85a4e
jefri ng <jefri.sae-tem@varmeego.com>
2022-06-01 17:11:40 +0800

    interceptor implement

--- src/main/java/com/example/java_backend_demo/Interceptor/UserTokenInterceptor.java
+++ src/main/java/com/example/java_backend_demo/Interceptor/UserTokenInterceptor.java
@@ -2,6 +2,7 @@ package com.example.java_backend_demo.Interceptor;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,5 +12,15 @@ public class UserTokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
