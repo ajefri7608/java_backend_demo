80c2fdd9c4d55ad8f1614f8e9229dc3da308a121
jefri ng <jefri.sae-tem@varmeego.com>
2022-07-04 15:22:32 +0800

    rsa decryption

--- src/main/java/com/example/java_backend_demo/encryption/CreateRsaKey.java
+++ src/main/java/com/example/java_backend_demo/encryption/CreateRsaKey.java
@@ -1,5 +1,7 @@
 package com.example.java_backend_demo.encryption;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,7 +11,8 @@ public class CreateRsaKey {
     private KeyPairGenerator generator;
     private PublicKey rsaPublicKey;
     private PrivateKey rsaPrivateKey;
-
+    private static String rsaPublicKeyPath = "./RSAPublicKey";
+    private static String rsaPrivateKeyPath = "./RSAPrivateKey";
     public CreateRsaKey() throws NoSuchAlgorithmException {
         generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(2048);
@@ -17,14 +20,14 @@ public class CreateRsaKey {
         rsaPublicKey = pair.getPublic();
         rsaPrivateKey = pair.getPrivate();
     }
-    public void writeKeyToFile(){
+    public void writeKeyToFile(String publicKeyPath, String privateKeyPath){
         try {
             if(rsaPublicKey != null && rsaPrivateKey != null){
-                FileOutputStream fileOutputStream = new FileOutputStream("./RSAPublicKey");
+                FileOutputStream fileOutputStream = new FileOutputStream(publicKeyPath);
                 fileOutputStream.write(rsaPublicKey.getEncoded());
                 fileOutputStream.flush();
                 fileOutputStream.close();
-                fileOutputStream = new FileOutputStream("./RSAPrivateKey");
+                fileOutputStream = new FileOutputStream(privateKeyPath);
                 fileOutputStream.write(rsaPrivateKey.getEncoded());
                 fileOutputStream.flush();
                 fileOutputStream.close();
@@ -49,7 +52,7 @@ public class CreateRsaKey {
     }
     public static void main(String[] args) throws NoSuchAlgorithmException {
         CreateRsaKey rsaEncrypt = new CreateRsaKey();
-        rsaEncrypt.writeKeyToFile();
+        rsaEncrypt.writeKeyToFile(rsaPublicKeyPath, rsaPrivateKeyPath);
 
     }
 
