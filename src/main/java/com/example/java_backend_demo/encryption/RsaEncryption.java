01eae6a7d7112fbfa50804287b3b2fdd86bae968
jefri ng <jefri.sae-tem@varmeego.com>
2022-07-05 14:00:38 +0800

    aes encryption

--- src/main/java/com/example/java_backend_demo/encryption/RsaEncryption.java
+++ src/main/java/com/example/java_backend_demo/encryption/RsaEncryption.java
@@ -6,6 +6,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -13,13 +14,16 @@ import java.security.*;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.*;
+import java.util.Random;
 
 public class RsaEncryption {
     private static String rsaPublicKeyPath = "./RSAPublicKey";
     private static String rsaPrivateKeyPath = "./RSAPrivateKey";
+
+    private static String algorithm = "RSA";
+
     public static PrivateKey getPrivateKeyFromFile(String privateKeyPath) throws Exception {
         byte[] key = Files.readAllBytes(new File(privateKeyPath).toPath());
-
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(key);
 
@@ -27,9 +31,7 @@ public class RsaEncryption {
     }
 
     public static PublicKey getPublicKeyFromFile(String publicKeyPath) throws Exception {
-
         byte[] key = Files.readAllBytes(new File(publicKeyPath).toPath());
-
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
         X509EncodedKeySpec spec = new X509EncodedKeySpec(key);
 
@@ -37,7 +39,7 @@ public class RsaEncryption {
     }
 
     public static String encrypt(String content) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance(algorithm);
         try{
             cipher.init(Cipher.ENCRYPT_MODE, getPublicKeyFromFile(rsaPublicKeyPath));
         }catch (Exception e){
@@ -50,7 +52,7 @@ public class RsaEncryption {
     }
 
     public static String decrypt(String encryptedContent) throws NoSuchPaddingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance(algorithm);
         try{
             cipher.init(Cipher.DECRYPT_MODE, getPrivateKeyFromFile(rsaPrivateKeyPath));
         }catch (Exception e){
@@ -65,10 +67,10 @@ public class RsaEncryption {
         signature.initSign(getPrivateKeyFromFile(rsaPrivateKeyPath));
         byte[] contentToBytes = content.getBytes(StandardCharsets.UTF_8);
         signature.update(contentToBytes);
-
-
         return signature.sign();
     }
+
+
 
     public static boolean verifySignature(String content, byte[] receivedSignature) throws Exception {
         Signature signature = Signature.getInstance("SHA256withRSA");
@@ -82,8 +84,7 @@ public class RsaEncryption {
         RsaEncryption rsaEncryption = new RsaEncryption();
         System.out.println(rsaEncryption.getPrivateKeyFromFile(rsaPrivateKeyPath).toString());
         System.out.println(rsaEncryption.getPublicKeyFromFile(rsaPublicKeyPath).toString());
-        System.out.println(decrypt(encrypt("ASDQWE")));
-        System.out.println("test1231   " + verifySignature("ASDQWE",signSignature("ASDQWE")));
+        System.out.println("verifySignature: " + verifySignature("ASDQWE",signSignature("ASDQWE")));
 
 
     }
