f19b2d7ed6fc3867dcf935c4e656aed5310d2ee8
jefri ng <jefri.sae-tem@varmeego.com>
2022-09-20 20:45:41 +0800

    image upload & display

--- src/main/java/com/example/java_backend_demo/controller/ProductVehicleController.java
+++ src/main/java/com/example/java_backend_demo/controller/ProductVehicleController.java
@@ -7,15 +7,39 @@ import com.example.java_backend_demo.Model.GeneralResponse;
 
 import com.example.java_backend_demo.service.ProductVehicleCreateService;
 import com.example.java_backend_demo.service.ProductVehicleSearchService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.servlet.ServletContext;
 import javax.validation.Valid;
+
 
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductVehicleController extends BaseController{
+    @Value("${SavePath.Image}")
+    private String ImagePath;
+
+    @Value("${SavePath.ImageMapper}")
+    private String ImageMapperPath;
+
+
 
     @Autowired
     ProductVehicleCreateService productVehicleCreateService;
@@ -32,6 +56,34 @@ public class ProductVehicleController extends BaseController{
     public GeneralResponse searchVehicle(@RequestBody ProductVehicleQueryParams productVehicleQueryParams) throws GeneralException {
         return productVehicleSearchService.start(productVehicleQueryParams);
     }
+    @PostMapping("imageUploadTest")
+    public String ImageUploadTest(@RequestParam("file") MultipartFile file){
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        fileName = UUID.randomUUID() + suffixName;
+
+        try{
+
+            file.transferTo(new File(ImagePath + fileName));
+            return ImageMapperPath + fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+
+    }
+    @GetMapping(value = "imageDisplayTest", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[]  imageDisplayTest(@RequestParam(name = "string") String filePath){
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(ImagePath + "/" + filePath));
+            return bytes;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 
 
 
